617be9c8162bdf494c72f15dcaaa1425e4b8d9d2
Scott Warchal <S.Warchal@sms.ed.ac.uk>
2017-09-28 20:56:24 +0100

    tab size

--- misc/fasta_parser.nim
+++ misc/fasta_parser.nim
@@ -4,23 +4,26 @@
 
 import os, strutils, tables
 
+
 proc parse_fasta(file: string):Table[string, string] =
+    var
+        f = open(file, fmRead)
+        store = initTable[string, string]()
+        name : string
+    
+    for line in f.lines:
+        if line.startswith(">"):
+            name = line[1..line.high]
+            store[name] = ""
+        else:
+            store[name].add(line)
+            
+    return store
 
-  var
-    f = open(file, fmRead)
-    store = initTable[string, string]()
-    name : string
 
-  for line in f.lines:
-    if line.startswith(">"):
-      name = line[1..line.high]
-      store[name] = ""
-    else:
-      store[name].add(line)
-
-  return store
 
 if paramCount() == 1:
-  echo parse_fasta(paramStr(1))
+    echo parse_fasta(paramStr(1))
 else:
-  echo parse_fasta("example.fasta")
+    echo parse_fasta("example.fasta")
+                    
